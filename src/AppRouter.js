7336909eeb01ae2db9554cbfbc20f3886ec287f7
Hasan Hüsamettin Eroğlu <hasanheroglu@gmail.com>
2020-01-12 00:30:50 +0300

    Added mainpage.

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
 import PostGet from './PostGet';
+import MainPage from './MainPage';
 
 const AppRouter = () =>(
     <BrowserRouter>
+        <Route exact path="/*" component={MainPage} />
         <Route exact path="/posts" component={PostGet} />
     </BrowserRouter>
 );
---
+++ src/MainPage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Navbar, Nav} from 'react-bootstrap';
+
+class MainPage extends React.Component{
+    constructor(props){
+      super(props)
+      this.state = {}
+    }
+
+    render(){  
+      utils.setRoles();
+        
+        return(
+          <div>
+            <Navbar bg="light" expand="lg">
+            <Navbar.Toggle aria- controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link>Home</Nav.Link>
+                <Nav.Link>About</Nav.Link>
+                <Nav.Link>Team</Nav.Link>
+                <Nav.Link>Contact</Nav.Link>
+                <Nav.Link href="/posts">Posts</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+            </Navbar>
+          </div>
+            
+        )
+    }
+}
+
+export default MainPage;
